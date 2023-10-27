762bd7763d3f565f62c47a688fa9cf6b94cda611
sarahkhaledd <61946261+sarahkhaledd@users.noreply.github.com>
2023-10-27 18:37:12 +0200

    Add getMinimum function

--- main.cpp
+++ main.cpp
@@ -16,12 +16,27 @@ double getAverage(const std::vector<int>& vector){
     return average;
 }
 
+int getMinimum(const std::vector<int>& vector){
+    if (vector.empty()) {
+        return std::numeric_limits<int>::max();
+    }
+    int minimum = vector[0];
+    for(int i : vector){
+        if(minimum > i)
+            minimum = i;
+    }
+    return minimum;
+}
+
 
 int main() {
     std::vector<int> myVector{1,4,3,2,5,6,8,7,9,10};
     int sum = getSum(myVector);
     double average = getAverage(myVector);
+    int minimum = getMinimum(myVector);
     std::cout << "the sum of vector elements.: " << sum << std::endl;
     std::cout << "the average of vector elements: " << average << std::endl;
+    std::cout << "the minimum of vector elements: " << minimum << std::endl;
+
     return 0;
 }
