edffa168aafefce5c5ce93b317e2159b22ac589d
sarahkhaledd <61946261+sarahkhaledd@users.noreply.github.com>
2023-10-27 18:00:39 +0200

    Add getSum function for vector summation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+int getSum(const std::vector<int>& vector){
+    int summation = 0;
+    for(int i : vector){
+        summation+=i;
+    }
+    return summation;
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> myVector{1,4,3,2,5,6,8,7,9,10};
+    int sum = getSum(myVector);
+
+    std::cout << "the sum of vector elements.: " << sum << std::endl;
     return 0;
 }
